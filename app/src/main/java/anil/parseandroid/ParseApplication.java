1d9c9259dd3455cee6641140f73f82d246f5c9bd
syllogismos <anilkaraka@live.in>
2014-10-13 09:37:27 +0530

    upgrade parse to 1.7.0 and add push service

--- app/src/main/java/anil/parseandroid/ParseApplication.java
+++ app/src/main/java/anil/parseandroid/ParseApplication.java
@@ -1,10 +1,14 @@
 package anil.parseandroid;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.parse.Parse;
 import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParsePush;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 /**
  * Created by Anil on 10/1/2014.
@@ -24,5 +28,16 @@ public class ParseApplication extends Application {
         defaultACL.setPublicReadAccess(true);
 
         ParseACL.setDefaultACL(defaultACL, true);
+
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
     }
 }
